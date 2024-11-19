e4e87aa7760212c8b9dfaa5a4b73e66cf0e07a85
the_jack_is_on_fire <alexsanderonfire@gmail.com>
2024-11-19 14:49:31 -0300

    tests-success

--- order/factories.py
+++ order/factories.py
@@ -1,17 +1,18 @@
-import factory # type: ignore
+import factory
 
 from django.contrib.auth.models import User
 from product.factories import ProductFactory
 
 from order.models import Order
 
-class UserFactory(factory.django.DjangoModelFactory):
-    username = factory.Faker('user_name')
-    email = factory.Faker('email')
 
+class UserFactory(factory.django.DjangoModelFactory):
+    email = factory.Faker('pystr')
+    username = factory.Faker('pystr')
 
     class Meta:
         model = User
+
 
 class OrderFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(UserFactory)
@@ -27,3 +28,4 @@ class OrderFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Order
+
